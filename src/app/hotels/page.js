68d3312d99833901dcefb248c773515052e0c64d
SaqlineShabbir <saqline5869@gmail.com>
2024-09-22 23:20:55 +0600

    hotel page added

---
+++ src/app/hotels/page.js
@@ -0,0 +1,23 @@
+import HotelList from "@/components/hotel/HotelList";
+import Filter from "@/components/search/Filter";
+import Search from "@/components/search/Search";
+
+const page = () => {
+  return (
+    <>
+      <section className=" bg-cover bg-no-repeat bg-center pt-[100px] pb-[60px]">
+        <div className="max-w-7xl mx-auto w-full px-4 items-center py-12 ">
+          <Search fromList={true} />
+        </div>
+      </section>
+      <section className="py-12">
+        <div className="max-w-7xl mx-auto w-full px-4 grid grid-cols-12">
+          <Filter />
+          <HotelList />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
