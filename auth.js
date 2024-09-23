3979a9daccb8e3f6dfbd5f2a97a83afc4b843b31
SaqlineShabbir <saqline5869@gmail.com>
2024-09-23 23:17:20 +0600

    credintial login implemented

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 import mongoClientPromise from "@/connectDb/mongoClientPromise";
+import { userModel } from "@/models/userModel";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/Facebook";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 export const {
   handlers: { GET, POST },
@@ -10,7 +12,37 @@ export const {
   signOut,
 } = NextAuth({
   adapter: MongoDBAdapter(mongoClientPromise, {}),
+  session: {
+    strategy: "jwt",
+  },
+
   providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+          console.log({ user });
+          if (user) {
+            const isMatch = user.email === credentials.email;
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Email or password mismatch");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,6 +1,31 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { login } from "../../app/actions";
 const LoginForm = () => {
+  const [error, setError] = useState("");
+  const router = useRouter();
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await login(formData);
+      console.log(response);
+      if (!!response.error) {
+        setError(response.error.message);
+      } else {
+        router.push("/bookings");
+      }
+    } catch (error) {
+      console.log(error);
+      setError(
+        error instanceof Error ? error.message : "An unexpected error occurred."
+      );
+    }
+  }
+
   return (
-    <form className="flex flex-col my-6">
+    <form className="flex flex-col my-6" onSubmit={onSubmit}>
       <div className="flex flex-col gap-2 my-2">
         <label htmlFor="email">Email Address</label>
         <input
@@ -20,10 +45,10 @@ const LoginForm = () => {
           id="password"
         />
       </div>
-
+      <span className="text-red-400">{error}</span>
       <button
         type="submit"
-        className="bg-green-400 px-8 py-2 rounded-md block  font-bold  hover:shadow active:scale-95 transition-all w-full mt-4"
+        className="bg-green-400 px-8 py-2 rounded-md block font-bold hover:shadow active:scale-95 transition-all w-full mt-4"
       >
         Login
       </button>
