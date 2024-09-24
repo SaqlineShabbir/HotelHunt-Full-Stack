c20d127056c26dd8c72f62315647a89b3a754aca
SaqlineShabbir <saqline5869@gmail.com>
2024-09-24 15:14:41 +0600

    changes

--- auth.js
+++ auth.js
@@ -2,8 +2,8 @@ import mongoClientPromise from "@/connectDb/mongoClientPromise";
 import { userModel } from "@/models/userModel";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
-import FacebookProvider from "next-auth/providers/Facebook";
 import CredentialsProvider from "next-auth/providers/credentials";
+import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 export const {
   handlers: { GET, POST },
